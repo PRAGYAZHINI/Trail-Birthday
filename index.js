3b1b9baff4a5eaa1503eac1abd22585a41b7a036
PRAGYAZHINI <69719146+PRAGYAZHINI@users.noreply.github.com>
2020-08-21 10:13:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 html { 
-  background: file:///C:/Users/HMM/Desktop/screen-3.jpg no-repeat center center fixed; 
+  background: /screen-3.jpg no-repeat center center fixed; 
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
