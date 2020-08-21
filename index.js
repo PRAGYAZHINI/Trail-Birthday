e9bed8fa648b40dbe6bad8c2bc9c2362ed3088c2
PRAGYAZHINI <69719146+PRAGYAZHINI@users.noreply.github.com>
2020-08-21 10:11:53 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+html { 
+  background: file:///C:/Users/HMM/Desktop/screen-3.jpg no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
